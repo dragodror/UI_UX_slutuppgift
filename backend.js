fdc9653ff635b068ea3e81921e582f734c8c5877
Philip <philipjonsson02@gmail.com>
2024-11-13 16:05:54 +0100

    working add to json

--- backend.js
+++ backend.js
@@ -1,32 +1,58 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 var data = require('./data/industrial.json');
 const listenPort = 3000;
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname)));
+
 app.listen(listenPort, () => {
     console.log("Started express on http://localhost:${listenPort}");
+});
+
+app.get('/', (req, res) => { //Set Start page to index.html
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.get('/items', (req, res) => {
     res.json(data);
 });
 
-app.get('/item/:articleNumber', (req, res) => {
-    const { articleNumber } = req.params;
-    for (let item of data) {
-        if (item.articleNumber === articleNumber) {
-            res.json(item);
-            return;
-        }
-    }
-    res.status(404).send();
-});
-
-app.post('/item', (req, res) => {
+app.post('/items', (req, res) => {
     const {name, articleNumber, price, description } = req.body;
     const item = {name, articleNumber, price, description};
     data.push(item);
     console.log("Created item ${articleNumber}");
     res.json(item);
 });
+
+const fs = require('fs');
+
+app.post('/orderWares', (req, res) =>{
+    const orderDetails = req.body;
+    console.log("hej order")
+    const orderFile = path.join(__dirname, 'data', 'myOrder.json');
+    
+    fs.readFile(orderFile, (err, data) => {
+        let orders = [];
+        orders = JSON.parse(data);
+
+        const newOrder = {
+            orderID: crypto.randomUUID(),
+            items: orderDetails
+        };
+
+        orders.push(newOrder);
+
+        fs.writeFile(orderFile, JSON.stringify(orders, null, 2), (err) =>{
+            if (err){
+                console.error('Error saving order');
+
+            }
+            else {
+                console.log('Order Saved');
+            }
+        })
+    });
+})
--- script.js
+++ script.js
@@ -1,7 +1,89 @@
-
-const name = [];
+const id = []
+const itemName = [];
 const articleNumber = [];
 const price = [];
 const description = [];
 
-const myData = fetch('')
+const response = fetch('http://localhost:3000/items')
+    .then(response => response.json())
+    .then(data => {
+        const itemID = Object.keys(data);
+        
+        console.log("item id is: " + itemID[0]);
+
+        for (let i of itemID) {
+            itemName.push(data[i].name);
+            articleNumber.push(data[i].articleNumber);
+            price.push(data[i].price);
+            description.push(data[i].description);
+
+            const myWare = document.getElementById("myWares");
+
+            const myWaresElement = document.createElement("div");
+            myWaresElement.id = "waresElement";
+            myWaresElement.className = "waresElement"
+            myWare.appendChild(myWaresElement);
+
+            let myItemName = document.createElement("p");
+            myItemName.id = "name_" + data[i].articleNumber;  
+            myItemName.innerHTML = data[i].name;
+            myWaresElement.appendChild(myItemName);
+
+            let myArticleNumber = document.createElement("p");
+            myArticleNumber.id = "article_" + data[i].articleNumber;
+            myArticleNumber.innerHTML = data[i].articleNumber;
+            myWaresElement.appendChild(myArticleNumber);
+
+            let myPriceTag = document.createElement("p");
+            myPriceTag.id = "price_" + data[i].articleNumber;
+            myPriceTag.innerHTML = data[i].price;
+            myWaresElement.appendChild(myPriceTag);
+
+            let myDescription = document.createElement("p");
+            myDescription.id = "description_" + data[i].articleNumber;
+            myDescription.innerHTML = data[i].description;
+            myWaresElement.appendChild(myDescription);
+
+            let myItemAmount = document.createElement("input");
+            myItemAmount.type = "number";
+            myItemAmount.id = "orderID_" + data[i].articleNumber; 
+            myWaresElement.appendChild(myItemAmount);
+        }
+    });
+
+const myButton = document.getElementById("orderButton");
+myButton.addEventListener('click', orderWares);
+
+function orderWares(){
+
+    const inputs = document.querySelectorAll("input[type='number']");
+    let orderDetails = [];
+
+    inputs.forEach(input =>{
+        const itemID = input.id.replace("orderID_", ""); // Get the article number
+        const quantity = parseInt(input.value); // Get the quantity
+
+        if (!isNaN(quantity) && quantity > 0) {
+            const itemIndex = articleNumber.indexOf(itemID);
+            
+            const item = {
+                name: itemName[itemIndex],
+                articleNumber: itemID,
+                price: price[itemIndex],
+                quantity: quantity
+            };
+
+            orderDetails.push(item);
+             //Input send to myOrder.json
+        }
+    });
+    fetch('http://localhost:3000/orderWares', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(orderDetails)
+    })
+    console.log(orderDetails);
+}
+    
